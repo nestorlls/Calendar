abea5100963c2ea3a7cb8356224b6181e62d5875
Paul Llanque <llanquepol@gmail.com>
2023-06-09 01:26:57 -0500

    add: contants

---
+++ constants.js
@@ -0,0 +1,25 @@
+export const constants = {
+  week: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ],
+  months: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+};
